545b0b9456882d3c2442bbaf0aea37daf2ae3ca4
Vaibhav <codingvaibhav247@gmail.com>
2024-03-25 03:43:39 +0530

    bugs

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bp = require('body-parser');
 const user = require('./models/userschema.js');
 const mongoose = require('mongoose');
 const path = require('path');
-const accountSid = 'AC2432ec38ded5be3df0578d9c77918fcf';
-const authToken = 'afaf98e80299af84c01583b3e886087c';
+// const accountSid = 'AC2432ec38ded5be3df0578d9c77918fcf';
+// const authToken = 'afaf98e80299af84c01583b3e886087c';
 const client = require('twilio')(accountSid, authToken);
 const mongoDB = 'mongodb+srv://httwarriors12:akshat@cluster0.n9sknas.mongodb.net/hacktt';
 const methodOverride = require("method-override");
@@ -298,16 +298,3 @@ app.listen(3000,()=>{
 
 
 
-//TWILIO
-// const accountSid = 'AC2432ec38ded5be3df0578d9c77918fcf';
-// const authToken = afaf98e80299af84c01583b3e886087c;
-// const client = require('twilio')(accountSid, authToken);
-
-// client.messages
-//     .create({
-//         body: 'Your appointment is coming up on July 21 at 3PM',
-//         from: 'whatsapp:+14155238886',
-//         to: 'whatsapp:+916387488465'
-//     })
-//     .then(message => console.log(message.sid))
-//     .done();
